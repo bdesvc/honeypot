ed5e217687663c96fbdbc5ee7e881520e0b597f6
Akex64 <61739633+Akex64@users.noreply.github.com>
2020-12-06 14:45:33 +0100

    Update strings.h

--- strings.h
+++ strings.h
@@ -3,3 +3,5 @@
 #include <string.h>
 
 const char* BusyBox = "\nBusyBox v1.16.1 (2013-07-28 16:09:21 CST) built-in shell (ash)\r\nEnter 'help' for a list of built-in commands.\r\n\n";
+const char* ListFiles = "\x1b[34mbin  boot  dev  etc  home  init  lib  \x1b[32mlib32  lib64  libx32  \x1b[34mmedia  mnt  opt  \x1b[32mproc  \x1b[34mroot  run  sbin  snap  srv  sys  tmp  \x1b[32musr  \x1b[34mvar\x1b[37m\r\n";
+const char* WGet = "wget: missing URL\r\nUsage: wget [OPTION]... [URL]...\r\n\r\nTry `wget --help' for more options.\r\n";
