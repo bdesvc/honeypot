68cbf63880aad457ce2ff2028fcff0215ecb827b
Akex64 <61739633+Akex64@users.noreply.github.com>
2020-12-09 13:10:22 +0100

    Update main.c

--- main.c
+++ main.c
@@ -13,101 +13,127 @@
 #include "strings.h"
 
 struct honeypot_t Honeypots[]; // define array for honeypot clients instead of passing structs
-char* Paths[]; // define current paths for each user/malware
+char* Paths[];
 
-int connCounter = 0; // define connection counter
+int connCounter = 0;
 
 void* System(int id)
 {
-	struct creds_t credentials; // initialize credentials
-	struct honeypot_t honeypot = Honeypots[id]; // get honeypot user info
 
-	char ConnBuffer[1024]; // define read buffer
-	read(honeypot.fd, &ConnBuffer, 1024); // read
+	FILE* filePtr;
+	filePtr = fopen("commands.log", "a");
 
-	write(honeypot.fd, "dvr login: ", strlen("dvr login: ")); // send dvr login
-	read(honeypot.fd, &credentials.username, 32); // read username
+	struct creds_t credentials;
+	struct honeypot_t honeypot = Honeypots[id];
 
-	Strip(&credentials.username); // strip username
+	char ConnBuffer[1024];
+	read(honeypot.fd, &ConnBuffer, 1024);
+
+	write(honeypot.fd, "dvr login: ", strlen("dvr login: "));
+	read(honeypot.fd, &credentials.username, 32);
+
+	Strip(&credentials.username);
 
 
 
-	write(honeypot.fd, "password: ", strlen("password: ")); // send password
-	read(honeypot.fd, &credentials.password, 32); // read password
+	write(honeypot.fd, "password: ", strlen("password: "));
+	read(honeypot.fd, &credentials.password, 32);
 
 
-	Strip(&credentials.password); // strip password
+	Strip(&credentials.password);
 
-	if(credentials.password[0] == '\0' || credentials.username[0] == '\0') // check if either strings are empty
-	{
-		write(honeypot.fd, "\n\nLogin failed\r\n", strlen("\n\nLogin failed\r\n")); // send login empty
-		sleep(3); // wait 3 seconds
-		shutdown(honeypot.fd, 2); // shutdown socket
-		pthread_exit(NULL); // kill thread
+	if(credentials.password[0] == '\0' || credentials.username == '\0'){
+		printf("[%i] [honeypot] authentication failed (%p)\n", time(NULL), honeypot.fd);
+		fprintf(filePtr, "[%i] [honeypot] authentication failed (%p)\n", time(NULL), honeypot.fd);
+		write(honeypot.fd, "\n\nLogin failed\r\n", strlen("\n\nLogin failed\r\n"));
+		sleep(3);
+		shutdown(honeypot.fd, 2);
+		pthread_exit(NULL);
 	}
 
-	Paths[honeypot.fd] = "/"; // set path
+	printf("[%i] [honeypot] authentication succeeded [%s:%s] (%p)\n", time(NULL), credentials.username, credentials.password, honeypot.fd);
+	fprintf(filePtr, "[%i] [honeypot] authentication succeeded [%s:%s] (%p)\n", time(NULL), credentials.username, credentials.password, honeypot.fd);
 
-	write(honeypot.fd, BusyBox, strlen(BusyBox)); // send busybox intro
+	Paths[honeypot.fd] = "/";
 
-	while(1) // while 1
+	write(honeypot.fd, BusyBox, strlen(BusyBox));
+
+
+
+	while(1)
 	{
-		char Data[1024]; // define buffer for input with size of 1024
+		char Data[1024];
 
-		SendPrompt(honeypot.fd, credentials.username, Paths[honeypot.fd]); // send prompt defined in utils.h
-		read(honeypot.fd, Data, 1024); // read data
+		SendPrompt(honeypot.fd, credentials.username, Paths[honeypot.fd]);
+		read(honeypot.fd, Data, 1024);
 
-		Strip(&Data); // strip data
+		Strip(&Data);
 
-		char* Args[1024]; // define char* array with size of 1024
+		char* Args[1024];
 
-		char* token = strtok(Data, " "); // find next " "
-		size_t i = 0; // define i as 0
-		while(token != NULL) // while token isn't null
+		char* token = strtok(Data, " ");
+		size_t i = 0;
+		while(token != NULL) 
 		{
-			Args[i] = token; // add argument to array
-			i++; // add 1 to i
-			token = strtok(NULL, " "); // find next token
+			Args[i] = token;
+			i++;
+			token = strtok(NULL, " ");
 		}
 
-		if(i != 0) // if arguments (i) is not 0
+		if(i != 0)
 		{
-			if(strstr(Args[0], "clear")) // if argument 0 contains clear
+			if(strstr(Args[0], "clear"))
 			{
-				write(honeypot.fd, "\033[2J\033[H", strlen("\033[2J\033[H")); // send ascii clear screen
-				write(honeypot.fd, BusyBox, strlen(BusyBox)); // send busybox intro
+				write(honeypot.fd, "\033[2J\033[H", strlen("\033[2J\033[H"));
+				write(honeypot.fd, BusyBox, strlen(BusyBox));
+				printf("[%i] [honeypot] clear called possibly normal user (%p)\n", time(NULL), honeypot.fd);
+				fprintf(filePtr, "[%i] [honeypot] clear called possibly normal user (%p)\n", time(NULL), honeypot.fd);
 			}
-			if(strstr(Args[0], "cd")) // if argument 0 contains cd
+			if(strstr(Args[0], "cd"))
 			{
-				if(i == 2) // if 1 argument was giving cd path, etc.
+				if(i == 2)
 				{
-					if(strstr(Args[1], "..")) // check if its dot to return
+					if(strstr(Args[1], ".."))
 					{
-						Paths[honeypot.fd] = "/"; // set path to default
+						printf("[%i] [honeypot] user changed directory to / (%p)\n", time(NULL), honeypot.fd);
+						fprintf(filePtr, "[%i] [honeypot] user changed directory to / (%p)\n", time(NULL), honeypot.fd);
+						Paths[honeypot.fd] = "/";
 					}
-					else // if its not .. set new path
+					else
 					{
-						Paths[honeypot.fd] = Args[1]; // set path to argument 1
+						printf("[%i] [honeypot] user changed directory to %s (%p)\n", time(NULL), Args[1], honeypot.fd);
+						fprintf(filePtr, "[%i] [honeypot] user changed directory to %s (%p)\n", time(NULL), Args[1], honeypot.fd);
+						Paths[honeypot.fd] = Args[1];
 					}
 				}
 			}
-			if(strstr(Args[0], "wget")) // if argument 0 contains wget
+			if(strstr(Args[0], "wget"))
 			{
-				write(honeypot.fd, WGet, strlen(WGet)); // send wget string defined in strings.h
+				printf("[%i] [honeypot] wget called with url %s (%p)\n", time(NULL), Args[1], honeypot.fd);
+				fprintf(filePtr, "[%i] [honeypot] wget called with url %s (%p)\n", time(NULL), Args[1], honeypot.fd);
+				write(honeypot.fd, WGet, strlen(WGet));
 			}
-			if(strstr(Args[0], "ls")) // if argument 0 contains ls
+			if(strstr(Args[0], "./"))
 			{
-				if(strstr(Paths[honeypot.fd], "/")) // write the list of fake files in / directory
+				printf("[%i] [honeypot] user tried to execute program '%s' (%p)\n", time(NULL), Args[0], honeypot.fd);
+				write(honeypot.fd, "\r\n", sizeof("\r\n"));
+			}
+			if(strstr(Args[0], "ls"))
+			{
+				printf("[%i] [honeypot] ls was called possibly normal user (%p)\n", time(NULL), honeypot.fd);
+				fprintf(filePtr, "[%i] [honeypot] ls was called possibly normal user (%p)\n", time(NULL), honeypot.fd);
+				if(strstr(Paths[honeypot.fd], "/"))
 				{
-					write(honeypot.fd, ListFiles, strlen(ListFiles)); // send fake files
+					write(honeypot.fd, ListFiles, strlen(ListFiles));
 				}
-				else // if its not /
+				else
 				{
-					write(honeypot.fd, "\r\n", strlen("\r\n")); // send newline because its a empty folder
+					write(honeypot.fd, "\r\n", strlen("\r\n"));
 				}
 			}
 		}
 	}
+	fclose(filePtr);
 }
 
 void* InitializeHoneypot(int port)
@@ -115,17 +141,15 @@ void* InitializeHoneypot(int port)
 	int listen_fd, connection_fd, length; // define sockets
 	struct sockaddr_in server_addr, client; // define server address and client address
 
-	printf("[honeypot] starting service..\n"); // print that honeypot service is starting to backend
+	printf("[%i] [honeypot] starting service..\n", time(NULL)); // print that honeypot service is starting to backend
 	listen_fd = socket(AF_INET, SOCK_STREAM, 0); // Initialize a socket for IPv4 via TCP
 	if(listen_fd == -1)  
 	{
-		printf("[honeypot] failed to create socket!\n"); // print that it failed to create socket to backend
+		printf("[%i] [honeypot] failed to create socket!\n", time(NULL)); // print that it failed to create socket to backend
 		exit(0); // shutdown process
 	}
 	else
-	{
-		printf("[honeypot] created socket!\n"); // print that it created a socket successfully to backend
-	}
+		printf("[%i] [honeypot] created socket!\n", time(NULL)); // print that it created a socket successfully to backend
 
 	bzero(&server_addr, sizeof(server_addr)); // zero server_addr's memory context
 
@@ -135,23 +159,19 @@ void* InitializeHoneypot(int port)
 
 	if((bind(listen_fd, (struct sockaddr*)&server_addr, sizeof(server_addr))) != 0) // bind the server address to the listening socket
 	{
-		printf("[honeypot] failed to bind!\n"); // print failed to bind to the backend
+		printf("[%i] [honeypot] failed to bind!\n", time(NULL)); // print failed to bind to the backend
 		exit(0); // shutdown process
 	}
 	else
-	{
-		printf("[honeypot] successfully binded to 0.0.0.0:%i\n", port); // print that it binded successfully to backend
-	}
+		printf("[%i] [honeypot] successfully binded to 0.0.0.0:%i\n", time(NULL), port); // print that it binded successfully to backend
 
 	if((listen(listen_fd, 10)) != 0) // Start listening
 	{
-		printf("[honeypot] failed to start listening!\n"); // prints that is failed to listen to backend
+		printf("[%i] [honeypot] failed to start listening!\n", time(NULL)); // prints that is failed to listen to backend
 		exit(0); // shutdown process
 	}
 	else
-	{
-		printf("[honeypot] started listening!\n"); // print that i started to listen
-	}
+		printf("[%i] [honeypot] started listening!\n", time(NULL)); // print that i started to listen
 
 	length = sizeof(client); // sets length to size of client address
 
@@ -182,11 +202,7 @@ void* InitializeHoneypot(int port)
 int main(int argc, char** argv)
 {
 	if(argc == 2)
-	{
 		InitializeHoneypot(atoi(argv[1])); // start on specified port
-	}
 	else
-	{
 		InitializeHoneypot(23); // start on default telnet port
-	}
 }
